86f271aea467a73bab6e6a5ba2e9d6c516c3a2ea
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-03-13 11:15:53 +0200

    project files

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+const app = require("./server.js");
+const PORT = process.env.PORT || 4000;
 
-const app =require("./server.js");
-const PORT =process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log(`server listening on ${PORT}`);
-  });
+  console.log(`server listening on ${PORT}`);
+});
--- server.js
+++ server.js
@@ -3,4 +3,25 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
-//Routes
+//Routers
+const projectRouters = require("./data/routers/project-routers");
+const actionRouters = require("./data/routers/action-routers");
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use(projectRouters);
+server.use(actionRouters);
+
+server.get("*", (req, res) => {
+  res.status(404).json("Page not found");
+});
+
+server.use((req, res) => {
+  res.json("This API is working and active");
+});
+
+module.exports = server;
